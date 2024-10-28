7da7780958c99964f861dd7f7483d5ed8985431f
vkoshcheev <ltsix2@gmail.com>
2024-10-28 20:55:35 +0300

    added new section - ShortBio

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import Credentials from './sections/Credentials/Credentials';
+import ShortBio from './sections/ShortBio/ShortBio';
 
 const App: React.FC = () => {
   return (
     <div className="main-container">
       <Credentials />
+      <ShortBio />
     </div>
   );
 };
