533533175949d7df64d875f93b99a726c166d67c
vkoshcheev <ltsix2@gmail.com>
2024-10-29 18:05:53 +0300

    Workplaces - added separators

--- src/sections/Workplaces/Workplaces.tsx
+++ src/sections/Workplaces/Workplaces.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import WorkplaceItemCollapsible from './components/WorkplaceItemCollapsible';
 import './Workplaces.scss';
 import { workplacesList } from './workplacesList';
@@ -5,15 +6,16 @@ import { workplacesList } from './workplacesList';
 const Workplaces = () => {
   return (
     <div className="section">
-      <div className="section__title">
-        Места работы:
-      </div>
+      <div className="section__title">Места работы:</div>
       <div className="workplaces-list__title-note">
         (нажмите на "+" чтобы посмотреть подробную информацию)
       </div>
-      <div className="workplaces-list">
-        {workplacesList.map((workplaceData) => (
-          <WorkplaceItemCollapsible key={workplaceData.startDate} workplaceItem={workplaceData} />
+      <div className="workplaces-list__items-container">
+        {workplacesList.map((workplaceData, index) => (
+          <React.Fragment key={workplaceData.startDate}>
+            {index !== 0 && <div className="workplaces-list__separator">↑</div>}
+            <WorkplaceItemCollapsible key={workplaceData.startDate} workplaceItem={workplaceData} />
+          </React.Fragment>
         ))}
       </div>
     </div>
