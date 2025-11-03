b72f8319889fa74d53fa283a7d23cfc876a4a52a
vkoshcheev <ltsix2@gmail.com>
2025-11-03 15:45:52 +0300

    add github & ffmpeg feature website links, improve Credentials.tsx design and structure

--- src/sections/Credentials/Credentials.tsx
+++ src/sections/Credentials/Credentials.tsx
@@ -1,52 +1,40 @@
 import media from '../../utils/media';
 import './Credentials.scss';
+import { linksInfo } from './linksInfo';
 
 const Credentials = () => {
   return (
-    <div className="section section-row">
-      <img src={media.profileImage} className="credentials-avatar" />
+    <div className="section">
+      <div className="credentials__main">
+        <img src={media.profileImage} className="credentials-avatar" />
 
-      <div className="credentials__main-info-block">
-        <div className="section__title">Владимир</div>
-        <div className="credentials__main-info-block__text">Frontend разработчик React</div>
+        <div className="credentials__main__info-block">
+          <div className="section__title">Владимир</div>
+          <div className="credentials__main__info-block__text">Front-end разработчик <b>React</b></div>
 
-        <div className="credentials__main-info-block__link-section">
-          <div className="show-above-720">Контакты можно найти на странице резюме hh.ru:<br /></div>
-          
-          <a
-            href="https://hh.ru/applicant/resumes/view?resume=fff97ac1ff0d88607e0039ed1f4a39617a3041"
-            target="_blank"
-          >
-            Ссылка на резюме (hh.ru)
-          </a>
+          <div className="show-above-720" style={{ marginTop: 'auto'}}>
+            <div className="credentials__main__info-block__links-section">
+              {linksInfo.map((linkInfo) => (
+                <div key={linkInfo.href}>
+                  <b>{linkInfo.title}</b>
+                  <br />
+                  <a href={linkInfo.href} target="_blank">
+                    {linkInfo.labelDesktop}
+                  </a>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
+      </div>
 
-        <div className="credentials__main-info-block__link-section show-above-720">
-          Исходный код:
-          <br />
-          <a href="https://github.com/vkoshcheev/resume" target="_blank">
-            https://github.com/vkoshcheev/resume
-          </a>
-        </div>
-
-        <div className="credentials__main-info-block__link-section show-above-720">
-          Мой вебсайт с курсами валют стран СНГ:
-          <br />
-          <a href="https://vkoshcheev.github.io/exchange-rates/" target="_blank">
-            https://vkoshcheev.github.io/exchange-rates
-          </a>
-        </div>
-
-        <div className="credentials__main-info-block__link-section show-below-720">
-          <a href="https://github.com/vkoshcheev/resume" target="_blank">
-            Ссылка на исходный код страницы
-          </a>
-        </div>
-
-        <div className="credentials__main-info-block__link-section show-below-720">
-          <a href="https://vkoshcheev.github.io/exchange-rates/" target="_blank">
-            Мой вебсайт - курсы валют стран СНГ
-          </a>
+      <div className="show-below-720">
+        <div className="credentials__main__info-block__links-section">
+          {linksInfo.map((linkInfo) => (
+            <a key={linkInfo.href} href={linkInfo.href} target="_blank">
+              {linkInfo.labelMobile}
+            </a>
+          ))}
         </div>
       </div>
     </div>
---
+++ src/sections/Credentials/linksInfo.ts
@@ -0,0 +1,32 @@
+export const linksInfo = [
+  {
+    title: 'Контакты можно найти на странице резюме hh.ru:',
+    labelDesktop: 'Ссылка на резюме (hh.ru)',
+    labelMobile: 'Ссылка на резюме (hh.ru)',
+    href: 'https://hh.ru/applicant/resumes/view?resume=fff97ac1ff0d88607e0039ed1f4a39617a3041',
+  },
+  {
+    title: 'Исходный код:',
+    labelDesktop: 'https://github.com/vkoshcheev/resume',
+    labelMobile: 'Ссылка на исходный код страницы',
+    href: 'https://github.com/vkoshcheev/resume',
+  },
+  {
+    title: 'Ссылка на github:',
+    labelDesktop: 'https://github.com/vkoshcheev',
+    labelMobile: 'Ссылка на github',
+    href: 'https://github.com/vkoshcheev',
+  },
+  {
+    title: 'Пет-проект - курсы валют стран СНГ:',
+    labelDesktop: 'https://vkoshcheev.github.io/exchange-rates/',
+    labelMobile: 'Пет-проект - курсы валют стран СНГ',
+    href: 'https://vkoshcheev.github.io/exchange-rates/',
+  },
+  {
+    title: 'Пет-проект - фича FFmpeg WebAssembly (Vercel):',
+    labelDesktop: 'https://remove-duplicate-frames.vercel.app/',
+    labelMobile: 'Пет-проект - фича FFmpeg WebAssembly',
+    href: 'https://remove-duplicate-frames.vercel.app/',
+  },
+];
